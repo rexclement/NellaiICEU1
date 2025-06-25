7e9280e41752cb73173c4a342944ec9debab822f
rexclement <146126821+rexclement@users.noreply.github.com>
2025-06-26 00:25:34 +0530

    Update index.js

--- myapp/server/index.js
+++ myapp/server/index.js
@@ -42,7 +42,9 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 
 app.use(session({
